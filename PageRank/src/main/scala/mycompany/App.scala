8bb08e5ed2886d23af2ed259de571fa5df910d67
Mark Garner <ms675199@rams.colostate.edu>
2022-11-07 13:13:34 -0700

    save changes

---
+++ PageRank/src/main/scala/mycompany/App.scala
@@ -0,0 +1,12 @@
+package mycompany
+
+// import org.apache.spark.sql.SparkSession
+
+object App {
+  
+  def main(args : Array[String]) {
+    println("Hello World!")
+    // val sc = SparkSession.builder().master("spark://pierre:30361").getOrCreate().sparkContext
+  }
+
+}
