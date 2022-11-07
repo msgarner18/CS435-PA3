2ba9ff7c675001973c02f1cda393a9fe88cbe949
Mark Garner <ms675199@rams.colostate.edu>
2022-11-07 16:16:47 -0700

    working pom file? I'm getting an error when running only

--- PageRank/src/main/scala/mycompany/App.scala
+++ PageRank/src/main/scala/mycompany/App.scala
@@ -1,12 +1,14 @@
-package mycompany
-
-// import org.apache.spark.sql.SparkSession
+package com.mycompany
+// import org.apache.spark
+import org.apache.spark.sql.SparkSession
 
 object App {
   
   def main(args : Array[String]) {
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     println("Hello World!")
     // val sc = SparkSession.builder().master("spark://pierre:30361").getOrCreate().sparkContext
+    spark.stop()
   }
 
 }
