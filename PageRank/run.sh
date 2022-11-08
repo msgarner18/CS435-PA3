7022687619d6132fcd206a0108f8f95c1b58b56f
Marti Selig <mselig@colostate.edu>
2022-11-07 18:52:31 -0700

    Added starting code

--- PageRank/run.sh
+++ PageRank/run.sh
@@ -1,2 +1,2 @@
 mvn package
-spark-submit target/PageRank-1.0-SNAPSHOT.jar
+$SPARK_HOME/bin/spark-submit target/PageRank-1.0-SNAPSHOT.jar --deploy-mode cluster --supervise ./target/PageRank-1.0-SNAPSHOT.jar
